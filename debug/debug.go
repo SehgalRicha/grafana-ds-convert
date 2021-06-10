568f12b7ace6d41ae1ff256858ed551ca22fea0a
rizkybiz <justin.defrank@circonus.com>
2021-06-10 10:37:27 -0400

    upd: introduced a debug function to simplify the code a bit

---
+++ debug/debug.go
@@ -0,0 +1,22 @@
+package debug
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+// PrintMarshal pretty prints a struct for use in debugging
+func PrintMarshal(msg string, v interface{}) {
+	log.Println(msg)
+	pp, _ := json.MarshalIndent(v, "", "    ")
+	fmt.Println(pp)
+}
+
+// PrintJSONBytes pretty prints a JSON byte array
+func PrintJSONBytes(msg, v []byte) {
+	var out bytes.Buffer
+	json.Indent(&out, v, "", "    ")
+	log.Printf("%s: %s\n", msg, out.String())
+}
